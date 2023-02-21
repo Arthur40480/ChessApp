9bf3b0f2543126075bbb1a01aa4549cdddbbae29
Arthur40480 <arthurgibert@free.fr>
2023-02-21 21:12:43 +0100

    Start  func for match/round

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,6 +1,8 @@
 from views.views import View
 from models.player import Player
 from tinydb import TinyDB, Query, where
+from datetime import datetime
+import random
 
 class Controller:
 
@@ -14,13 +16,13 @@ class Controller:
         self.user = Query()
 
     def display_title(self):
-        """ Fait appraître le titre """
+        """ Fait apparaître le titre """
         welcom_title = "Bienvenue dans l'application de tournois d'echecs ♟️"
         self.view.message(welcom_title)
         self.display_menu()
 
     def display_menu(self):
-        """ Fait appraître le menu """
+        """ Fait apparaître le menu """
         user_choice = self.view.menu()
         if user_choice == 1:
             self.new_player()
@@ -32,7 +34,7 @@ class Controller:
     """ ----- ----- Joueur ----- ----- """
     def new_player(self):
         """ Créer un nouveau Joueur """
-        player_creation_title = "Création d'un nouveau joueur 🙋"
+        player_creation_title = "🙋 Création d'un nouveau joueur 🙋"
         self.view.message(player_creation_title)
         data_player = self.view.new_player()
         first_name = data_player[0]
@@ -44,13 +46,13 @@ class Controller:
         same_name = self.players_table.search(self.user.first_name == first_name)
         same_last_name = self.players_table.search(self.user.last_name == last_name)
         if same_last_name and same_name:
-            error_message = "Ce joueur est déjà enregistrer ⚠️"
+            error_message = "⚠️ Ce joueur est déjà enregistrer ⚠️"
             self.view.message(error_message)
             self.display_menu()
 
         player = Player(last_name, first_name, dath_of_birth, rank)
         self.add_player_in_file(player.__dict__)
-        new_player_again = input("Voulez vous enregistrer un autre joueur ? 🔄 (Y/N) :")
+        new_player_again = input("🔄 Voulez vous enregistrer un autre joueur ? 🔄 (Y/N) :")
         if (new_player_again == "Y"):
             self.new_player()
         else:
@@ -74,12 +76,12 @@ class Controller:
     def add_player_in_file(self, data):
         """ Enregistre le joueur dans le fichier JSON """
         self.players_table.insert(data)
-        add_player_in_file_title = "Joueur bien enregistrer 🎉"
+        add_player_in_file_title = "🎉 Joueur bien enregistrer 🎉"
         self.view.message(add_player_in_file_title)
 
     def display_player_list(self):
         """ Affiche la liste des joueurs enregistrer par ordre alphabétique (Nom) """
-        list_title = "Liste des joueurs enregistrés 📃"
+        list_title = " 📃 Liste des joueurs enregistrés 📃"
         self.view.message(list_title)
         players = sorted(self.players_table, key=lambda player: player['last_name'])
         self.view.display_player_list(players)
@@ -87,7 +89,7 @@ class Controller:
     """ ----- ----- TOURNOI ----- ----- """
     def new_tournament(self):
         """ Créer un Tournoi """
-        tournament_creation_title = "Création d'un nouveau tournoi 🏁"
+        tournament_creation_title = "🏁 Création d'un nouveau tournoi 🏁"
         self.view.message(tournament_creation_title)
         tournament_data = self.view.new_tournament()
         name = tournament_data[0]
@@ -100,7 +102,8 @@ class Controller:
         self.tournament_table = self.db_tournament.table(f"{name}")
 
     def tournament_roster(self):
-        roster_title = " Veuillez choisir les joueurs participants au tournoi (8 joueurs max) 🏆"
+        """ Permet la séléction des joueurs participant au tournoi """
+        roster_title = "🏆 Veuillez choisir les joueurs participants au tournoi (8 joueurs max) 🏆"
         self.view.message(roster_title)
         roster_list = []
         players = self.players_roster()
@@ -110,11 +113,56 @@ class Controller:
                 number_select = int(input("Veuillez ajouter le numéro du joueur séléctionner :"))
                 player_select = (number_select - 1)
                 roster_list.append(players[player_select])
-                print(roster_list)
             except ValueError:
-                error_title = "Ceci n'est pas un numéro, veuillez ajouter un numéro ❗"
+                error_title = "❗ Ceci n'est pas un numéro, veuillez ajouter un numéro ❗"
                 self.view.message(error_title)
-        return roster_list
+        self.start_tournament(roster_list)
+
+    def start_tournament(self, roster_list):
+        """ Lance le tournoi """
+        answer = self.view.start_tournament()
+        if (answer == "Y"):
+            self.play_first_round(roster_list)
+        else:
+            self.display_menu()
+
+    """ ----- ----- ROUND ----- ----- """
+    def play_first_round(self, roster_list):
+        """ Lance le premier round """
+        start_date = datetime.today().strftime('%d/%m/%Y-%H:%M:%S')
+        self.view.play_first_round(start_date)
+        match_list = self.mix_player(roster_list)
+        for match in match_list:
+            self.play_match(match)
+
+    def mix_player(self, roster_list):
+        """ Mélange les joueurs, et définit les matchs (joueur vs joueur) sous forme de liste"""
+        random.shuffle(roster_list)
+        size = 2
+        sub_list = [roster_list[x: x + size] for x in range(0, len(roster_list), size)]
+        return sub_list
+
+    """ ----- ----- MATCHS ----- ----- """
+    def play_match(self, match_list):
+        """ Lance les matchs entre joueurs """
+        first_player = match_list[0]["last_name"] + match_list[0]["first_name"]
+        second_player = match_list[1]["last_name"] + match_list[1]["first_name"]
+        first_player_score = 0
+        second_player_score = 0
+        self.view.match_title(first_player, second_player)
+        self.color_draw(first_player, second_player)
+        self.view.end_match()
+        result = self.view.match_result(first_player, second_player)
+        print(result)
+
+    def color_draw(self, first_player, second_player):
+        """ Assignation des couleurs aléatoirement """
+        color = ["Blanc ⚪", "Noir ⚫"]
+        random.shuffle(color)
+        self.view.color_draw(first_player, second_player, color)
+
+
+
 
 
 
--- views/views.py
+++ views/views.py
@@ -10,7 +10,7 @@ class View:
 
     def menu(self):
         print("1: Ajouter un joueur ")
-        print("2: Liste de tous les joueurs ")
+        print("2: Liste des joueurs ")
         print("3: Créer un tournoi ")
         print("4: Liste des tournois")
         print("5: Quittez l'application")
@@ -21,14 +21,15 @@ class View:
             if user_choice > 0 and user_choice <= 6:
                 return user_choice
             else:
-                error_message = "Veuillez choisir un nombre compris dans la liste ⚠️"
+                error_message = "⚠️ Veuillez choisir un nombre compris dans la liste ⚠️"
                 self.message(error_message)
 
         except ValueError:
             """ Except nous permet de gérer une erreur 
             ➡ Si l'utilisateur rentre d'autres caractères que des nombres"""
-            error_message = "Veuillez choisir un nombre 🔢️"
+            error_message = "🔢️ Veuillez choisir un nombre 🔢️"
             self.message(error_message)
+
 
     """ ----- ----- JOUEUR ----- ----- """
     def new_player(self):
@@ -67,6 +68,51 @@ class View:
             print(str(x) + " " + "➡" + " " + player["last_name"] + " " + player["first_name"] + " " + str(player["rank"]) )
         print("-----------------------------------")
 
+    def start_tournament(self):
+        start_title = "🚥 Voulez vous commencer le tournoi ? 🚥"
+        self.message(start_title)
+        answer = input(" (Y/N) :")
+        return  answer
+
+
+    """ ----- ----- ROUND ----- ----- """
+    def play_first_round(self, start_date):
+       first_round_title = f" 🎌 Round 1  {start_date} 🎌"
+       self.message(first_round_title)
+
+
+    """ ----- ----- MATCH ----- ----- """
+    def match_title(self, first_player, second_player):
+        print(f"💥 Joueur 1 - {first_player} contre Joueur 2 - {second_player} 💥")
+
+    def color_draw(self, first_player, second_player, color):
+        print("🎲 Tirage au sort pour les couleurs ! 🎲")
+        print("---------------------")
+        print(f"Joueur 1: {first_player} jouera les {color[0]} !")
+        print(f"Joueur 2: {second_player} jouera les {color[1]} !")
+        print("---------------------")
+        print("🔥 Le match commence ! 🔥")
+
+    def match_result(self, first_player, second_player):
+        print("📣 Fin du match ! 📣")
+        print("---------------------")
+        print("Résultat du match ?")
+        print(f"1 - Joueur 1 - {first_player} vainqueur !")
+        print(f"2 - Joueur 2 - {second_player} vainqueur !")
+        print(f"3 - Egalité parfaite !")
+        try:
+            result = int(input("Veuillez entrer le résultat du match (1/2/3) :"))
+            if result > 0 and result <= 3:
+                return result
+            else:
+                error_message = "⚠️ Veuillez choisir un réultat correct ⚠️"
+                self.message(error_message)
+        except ValueError:
+            error_message = "🔢️ Veuillez choisir un nombre (1/2/3) 🔢️"
+            self.message(error_message)
+
+    def end_match(self):
+        input("🕦 Veuillez appuyer sur Entrer lorsque le match est terminer 🕦")
 
 
 
